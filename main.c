720110ae5429e6819359dc080cdde040245bdda8
hazemKhaled12 <hazem.khaled1293@gmail.com>
2023-08-23 21:37:17 +0000

    a

--- main.c
+++ main.c
@@ -3,24 +3,23 @@
 /**
  * main - Entry point
  *
- * @ac: arg count
- * @av: arg vector
+ * @argc: arg count
+ * @argv: arg vector
  * Return: Always 0.
  */
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    if(argc < 2)
-    {
-        printf("Give a filename\n");
-        return (1);
-    }
-    if (access(argv[1], F_OK) == -1)
-    {
-        printf("File does not exist\n");
-        return (2);
-    }
-
-    printf("Hello world %i,  %s , %s\n",argc, argv[0], argv[1]);
-    return (0);
+	if (argc < 2)
+	{
+		printf("Give a filename\n");
+		return (1);
+	}
+	if (access(argv[1], F_OK) == -1)
+	{
+		printf("File does not exist");
+		return (2);
+	}
+	printf("Hello world %i,  %s , %s\n", argc, argv[0], argv[1]);
+	return (0);
 }
