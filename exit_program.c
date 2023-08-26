0e3189c2c890ff38b4196c5fa2cb24cf7d4d08f9
hazem Khaled <hazem.khaled1293@gmail.com>
2023-08-26 02:28:40 +0200

    exit implementaion

--- exit_program.c
+++ exit_program.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * exit_program - Exit terminal
+ *
+ * Description: 'Exit terminal'
+ * @command: Command
+ * Return: Return void.
+ */
+
 void exit_program(const char *command)
 {
   if (strcmp(command, "exit") == 0)
